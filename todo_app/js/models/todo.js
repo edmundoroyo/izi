be9191dbd84d7300e8f107a70809339059b0c89d
EMEAAD\eroyobjelke <e@h.com>
2017-10-10 13:50:06 +0200

    Update del estado ya funciona. Queda pendiente eliminar y modificar texto

--- todo_app/js/models/todo.js
+++ todo_app/js/models/todo.js
@@ -1,6 +1,6 @@
 /*global Backbone */
 var app = app || {};
-var userId = 1;
+var userId = 'http://localhost:8080/SpringBootCRUDApp/users/1';
 
 (function() {
   'use strict';
@@ -17,7 +17,30 @@ var userId = 1;
       title: '',
       completed: false
     },
+	
+	parse: function(data){
+		
+		data.id = 0;
+		return data;
+		
+	},
+	
+    url: function() {
+		
+		if (this.attributes._links != null){
 
+			var x = this.attributes._links.self.href;
+			this.set('id',0);
+			return x;
+		
+		}else {
+		
+			return 'http://localhost:8080/SpringBootCRUDApp/duties'	
+			
+		}
+		
+	},
+	
     // Toggle the `completed` state of this todo item.
     toggle: function() {
       this.save({
