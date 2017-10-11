49acb814234e72ef43193bc8d30fb8bfde27c31e
Etienne Aribaud <etienne.aribaud@sopra.com>
2017-10-11 17:36:04 +0200

    fix(common.js): fix url

--- todo_app_require/js/common.js
+++ todo_app_require/js/common.js
@@ -6,8 +6,8 @@ define([], function() {
     TodoFilter: '',
     ENTER_KEY: 13,
     ESCAPE_KEY: 27,
-    URL_ROOT: 'http://192.168.0.73:8080/SpringBootCRUDApp/',
-    URL_DUTIES: 'http://192.168.0.73:8080/SpringBootCRUDApp/duties',
-    URL_USER: 'http://192.168.0.73:8080/SpringBootCRUDApp/users/1'
+    URL_ROOT: 'http://localhost:8080/SpringBootCRUDApp/',
+    URL_DUTIES: 'http://localhost:8080/SpringBootCRUDApp/duties',
+    URL_USER: 'http://localhost:8080/SpringBootCRUDApp/users/1'
   };
 });
